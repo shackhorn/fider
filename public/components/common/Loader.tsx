1c1590ea3258f56fb83ab1c273f328323a73c23a
goenning <me@goenning.net>
2021-04-11 21:10:38 +0100

    loader

--- public/components/common/Loader.tsx
+++ public/components/common/Loader.tsx
@@ -1,5 +1,3 @@
-import "./Loader.scss"
-
 import React, { useState } from "react"
 import { useTimeout } from "@fider/hooks"
 
@@ -8,7 +6,58 @@ export function Loader() {
 
   useTimeout(() => {
     setShow(true)
-  }, 500)
+  }, 200)
 
-  return show ? <div className="c-loader" /> : null
+  return show ? (
+    <div className="text-center">
+      <svg xmlns="http://www.w3.org/2000/svg" className="h-12 w-12 inline" viewBox="0 0 44 44" stroke="currentColor">
+        <g fill="none" fillRule="evenodd" strokeWidth="2">
+          <circle cx="22" cy="22" r="1">
+            <animate
+              attributeName="r"
+              begin="0s"
+              dur="1.8s"
+              values="1; 20"
+              calcMode="spline"
+              keyTimes="0; 1"
+              keySplines="0.165, 0.84, 0.44, 1"
+              repeatCount="indefinite"
+            />
+            <animate
+              attributeName="stroke-opacity"
+              begin="0s"
+              dur="1.8s"
+              values="1; 0"
+              calcMode="spline"
+              keyTimes="0; 1"
+              keySplines="0.3, 0.61, 0.355, 1"
+              repeatCount="indefinite"
+            />
+          </circle>
+          <circle cx="22" cy="22" r="1">
+            <animate
+              attributeName="r"
+              begin="-0.9s"
+              dur="1.8s"
+              values="1; 20"
+              calcMode="spline"
+              keyTimes="0; 1"
+              keySplines="0.165, 0.84, 0.44, 1"
+              repeatCount="indefinite"
+            />
+            <animate
+              attributeName="stroke-opacity"
+              begin="-0.9s"
+              dur="1.8s"
+              values="1; 0"
+              calcMode="spline"
+              keyTimes="0; 1"
+              keySplines="0.3, 0.61, 0.355, 1"
+              repeatCount="indefinite"
+            />
+          </circle>
+        </g>
+      </svg>
+    </div>
+  ) : null
 }
