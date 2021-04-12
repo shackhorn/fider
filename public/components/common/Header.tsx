eb94e7c4080d4b8b486bcd9047829993758f6329
goenning <me@goenning.net>
2021-04-12 21:12:00 +0100

    fix header

--- public/components/common/Header.tsx
+++ public/components/common/Header.tsx
@@ -1,13 +1,13 @@
 import React, { useState, useEffect } from "react"
 import { SignInModal, DevBanner, Avatar, TenantLogo } from "@fider/components"
 import { actions } from "@fider/services"
-import { FaUser, FaCog, FaCaretDown } from "react-icons/fa"
 import { useFider } from "@fider/hooks"
 import { HStack } from "./layout"
 
 export const Header = () => {
   const fider = useFider()
   const [isSignInModalOpen, setIsSignInModalOpen] = useState(false)
+  const [isUserMenuOpen, setIsUserMenuOpen] = useState(false)
   const [unreadNotifications, setUnreadNotifications] = useState(0)
 
   useEffect(() => {
@@ -28,35 +28,36 @@ export const Header = () => {
 
   const hideModal = () => setIsSignInModalOpen(false)
 
-  const items = fider.session.isAuthenticated && (
-    <div className="c-menu-user">
-      <div className="c-menu-user-heading">
-        <FaUser /> <span>{fider.session.user.name}</span>
+  const toggleUserMenu = () => {
+    setIsUserMenuOpen(!isUserMenuOpen)
+  }
+
+  const items = isUserMenuOpen && (
+    <div className="origin-top-right absolute text-left right-0 mt-2 z-10 rounded-md shadow-lg bg-white divide-y">
+      <div>
+        <div className="text-sm px-4 py-2 font-medium text-gray-900 truncate">
+          <span>{fider.session.user.name}</span>
+        </div>
+        <a href="/settings" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900">
+          Settings
+        </a>
       </div>
-      <a href="/settings" className="c-menu-user-item">
-        Settings
-      </a>
-      <a href="/notifications" className="c-menu-user-item">
-        Notifications
-        {unreadNotifications > 0 && <div className="c-unread-count">{unreadNotifications}</div>}
-      </a>
-      <div className="c-menu-user-divider" />
-      {fider.session.user.isCollaborator && [
-        <div key={1} className="c-menu-user-heading">
-          <FaCog /> <span>Administration</span>
-        </div>,
-        <a key={2} href="/admin" className="c-menu-user-item">
-          Site Settings
-        </a>,
-        <div key={5} className="c-menu-user-divider" />,
-      ]}
-      <a href="/signout?redirect=/" className="c-menu-user-item signout">
+      <div>
+        {fider.session.user.isCollaborator && [
+          <div key={1} className="text-sm px-4 py-2 font-medium text-gray-900 truncate">
+            <span>Administration</span>
+          </div>,
+          <a key={2} href="/admin" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900">
+            Site Settings
+          </a>,
+        ]}
+      </div>
+      <a href="/signout?redirect=/" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900 signout">
         Sign out
       </a>
     </div>
   )
 
-  const showRightMenu = fider.session.isAuthenticated || !fider.session.tenant.isPrivate
   return (
     <>
       <DevBanner />
@@ -70,13 +71,34 @@ export const Header = () => {
                 <h1 className="text-xl font-semibold text-gray-700">{fider.session.tenant.name}</h1>
               </HStack>
             </a>
-            {showRightMenu && (
+            {fider.session.isAuthenticated && (
+              <HStack center={true}>
+                <a href="/notifications" className="bg-white p-1 rounded-full text-gray-500 hover:text-gray-700">
+                  <div className="inline-block relative align-middle">
+                    <svg className="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
+                      ></path>
+                    </svg>
+                    {unreadNotifications > 0 && (
+                      <span className="absolute animate-pulse top-0 right-0 block h-2 w-2 rounded-full ring-2 ring-white bg-red-600"></span>
+                    )}
+                  </div>
+                </a>
+                <div>
+                  <button className="rounded-full cursor-pointer" onClick={toggleUserMenu}>
+                    <Avatar user={fider.session.user} />
+                  </button>
+                  {items}
+                </div>
+              </HStack>
+            )}
+            {!fider.session.isAuthenticated && !fider.session.tenant.isPrivate && (
               <div onClick={showModal} className="flex items-center">
-                {fider.session.isAuthenticated && <Avatar user={fider.session.user} />}
-                {unreadNotifications > 0 && <div className="c-unread-dot" />}
                 {!fider.session.isAuthenticated && <span className="font-medium cursor-pointer text-sm uppercase align-middle">Sign in</span>}
-                {fider.session.isAuthenticated && <FaCaretDown />}
-                {items}
               </div>
             )}
           </div>
